b087dc72be01ac7e1d009954109f279f82b7bc6f
SuzanneRashka <suzanne.rashka@gmail.com>
2016-12-21 09:41:57 -0600

    first commit

---
+++ app.js
@@ -0,0 +1,4 @@
+process.env.NODE_ENV = 'test';
+
+const app = require('/app');
+const knex = require('/knex');
---
+++ knexfile.js
@@ -0,0 +1,9 @@
+'use strict';
+
+module.exports = {
+    test: {
+        client: 'pg',
+        connection: 'postgres://localhost/knex_sloths_test',
+        debug: true
+    }
+};
